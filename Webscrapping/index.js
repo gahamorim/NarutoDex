2b5e351c967a92576a70726ec8bcce7d791f767b
Gabriel <Amorim>
2021-02-14 17:03:39 -0300

    scrapping data from characters

--- Webscrapping/index.js
+++ Webscrapping/index.js
@@ -2,8 +2,9 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    let characterData = [];
 
     try {
         for (var i = 65; i <= 90; i++) {
@@ -11,9 +12,19 @@ const fs = require('fs');
             const characterHtmlList = await page.$('#mw-content-text > div.category-page__members > ul:nth-child(2)');
             const eachCharacterInHtml = await characterHtmlList.$$('.category-page__member > a')
 
-            for (let i = 1; i <= [...eachCharacterInHtml].length; i++) {
-                page.click(`#mw-content-text > div.category-page__members > ul:nth-child(2) > li:nth-child(${i}) > a`)
+            for (let j = 1; j <= [...eachCharacterInHtml].length; j++) {
+                page.click(`#mw-content-text > div.category-page__members > ul:nth-child(2) > li:nth-child(${j}) > a`)
                 await page.waitForNavigation()
+                characterData.push(
+                    await page.evaluate(() => {
+                    const data = {};
+                    [...document.getElementsByClassName('pi-item pi-data pi-item-spacing pi-border-color')]
+                        .forEach((a) => {
+                            data[a.firstElementChild.innerText]= a.lastElementChild.innerText
+                        })
+                    return data;
+                })
+                )
                 await page.goBack()
             }
         }
@@ -21,10 +32,13 @@ const fs = require('fs');
         console.log(error)
     }
 
-    // fs.writeFile('narutoExample.json', JSON.stringify(nameList, null, 2), err => {
-    //     if (err) throw new Error('Something went wrong')
-    //     console.log('Well done!')
-    // })
+    fs.writeFile('narutoExample.json', JSON.stringify(characterData, null, 2), err => {
+        if (err) throw new Error('Something went wrong')
+        console.log('Well done!')
+    })
 
     await browser.close();
 })();
+
+// [...document.getElementsByClassName('pi-item pi-data pi-item-spacing pi-border-color')]
+// .map((a) => a.firstElementChild.parentElement.innerText)
